042c94afe970f7e382234f4cc3d063a3b66bc16f
evagolabek <golabek.eva@gmail.com>
2018-10-28 10:30:19 +0100

    blog post with mockdata display

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BLOGPOSTS } from '../../mockdata'
 
 @Component({
   selector: 'app-blog',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
-
+   
+  blogposts = BLOGPOSTS; 
   
   constructor() { }
 
--- src/mockdata.ts
+++ src/mockdata.ts
@@ -1,7 +1,7 @@
 import { Blogpost } from './blogpost';
 
 export const BLOGPOSTS: Blogpost [] = [
-  { title: 'LOGIN WITH YOUR OFFICE365 ACCOUNT', description: 'If you have an Office365 (Microsoft) account you can use it to log in to the Joheco customer portal.', imageUrl: 'assets/login-ms.png' },
+  { title: 'LOGIN WITH YOUR OFFICE365 ACCOUNT', description: 'If you have an Office365 (Microsoft) account you can use it to log in to the Joheco customer portal.', imageUrl: '../../assets/login-ms.png' },
   
-  { title: 'LOGIN WITH YOUR LINKEDIN ACCOUNT', description: 'Logging in to Johecos customer portal is also possible with your LinkedIn account.', imageUrl: 'assets/login-li.png' },
+  { title: 'LOGIN WITH YOUR LINKEDIN ACCOUNT', description: 'Logging in to Johecos customer portal is also possible with your LinkedIn account.', imageUrl: '../../assets/login-li.png' },
 ];
