214999f7eb988b89ecdb05df004f5f16f5623dae
evagolabek <golabek.eva@gmail.com>
2018-11-10 20:39:49 +0100

    dialog content implementation on question ts file

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
 
+//you need reference to the dialog to be able to close it, to get it you have to import MatDialogRef, the you are able to inject the mddialogref object to the constructor (declare the type MatDialogRef and bc its a type object so you need <QuestionsComponent> the you are able to use the close message)
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public thisDialogRef: MatDialogRef<QuestionsComponent> ) { }
 
   ngOnInit() {
+  }
+
+  onCloseConfirm(){
+    this.thisDialogRef.close('Confirm');
+  }
+
+  onCloseCancel(){
+    this.thisDialogRef.close('Cancel');
   }
 
 }
